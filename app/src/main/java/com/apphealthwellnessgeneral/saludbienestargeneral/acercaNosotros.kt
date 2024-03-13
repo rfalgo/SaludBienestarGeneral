fe2b0482558e313a419c9cd879fc46de60698e67
Robinson <robaldgom@gmail.com>
2024-03-13 18:16:46 -0500

    Sprint 1: User Interface Development

---
+++ app/src/main/java/com/apphealthwellnessgeneral/saludbienestargeneral/acercaNosotros.kt
@@ -0,0 +1,11 @@
+package com.apphealthwellnessgeneral.saludbienestargeneral
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class acercaNosotros : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_acerca_nosotros)
+    }
+}
---
+++ app/src/main/java/com/apphealthwellnessgeneral/saludbienestargeneral/alimentacion.kt
@@ -0,0 +1,11 @@
+package com.apphealthwellnessgeneral.saludbienestargeneral
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class alimentacion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alimentacion)
+    }
+}
