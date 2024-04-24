82daa170b287cbe82045ee0109c5857885eca10c
rfalgo <robaldgom@gmail.com>
2024-04-24 11:15:35 -0500

    Sprint 3: Implementation of advanced functionalities.

--- app/src/main/java/com/apphealthwellnessgeneral/saludbienestargeneral/asesoria.kt
+++ app/src/main/java/com/apphealthwellnessgeneral/saludbienestargeneral/asesoria.kt
@@ -1,11 +1,25 @@
 package com.apphealthwellnessgeneral.saludbienestargeneral
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class asesoria : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_asesoria)
     }
+
+    fun EnterAsincronica(view: View){
+        val intent= Intent(this, asesoriaA::class.java) .apply {  }
+        startActivity(intent)
+    }
+
+    fun EnterSincronica(view: View){
+        val intent= Intent(this, asesoriaS::class.java) .apply {  }
+        startActivity(intent)
+    }
+
+
 }
